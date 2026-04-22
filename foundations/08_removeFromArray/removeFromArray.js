e2de2a7bdca31705c63fe08a18cec61b41f8f575
Josh Russell <255812605+DeuceDagger@users.noreply.github.com>
2026-04-21 20:55:46 -0600

    Finish exercise 08
    Finish exercise 08 by creating array of arguments, splitting
    the array into the main array and the remaining arguments,
    then filtering the main array for any that "indexOf"
    in args is less than 0 (not found)

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  let args = Array.from(arguments);
+  let mainArray = args[0];
+  return mainArray.filter((item) => args.indexOf(item) < 0);
 };
 
 // Do not edit below this line
