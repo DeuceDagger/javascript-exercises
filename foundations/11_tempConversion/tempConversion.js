a1292b2338c176661b7e3226a36258d5491760af
Josh Russell <255812605+DeuceDagger@users.noreply.github.com>
2026-04-25 18:48:31 -0600

    Add conversion C to F and make formulas readable
    This update adds conversion of C to F. Rounding is still
    not yet in place.
    
    Also added additional parentheses to make things more
    readable

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -2,7 +2,9 @@ const convertToCelsius = function (temp) {
   return temp * (9 / 5) + 32;
 };
 
-const convertToFahrenheit = function (temp) {};
+const convertToFahrenheit = function (temp) {
+  return (temp - 32) * (5 / 9);
+};
 
 // Do not edit below this line
 module.exports = {
