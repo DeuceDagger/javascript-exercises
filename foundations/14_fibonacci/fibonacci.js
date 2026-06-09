1c3df8a77dc916799e643774149061323d26a65e
Josh Russell <255812605+DeuceDagger@users.noreply.github.com>
2026-06-09 17:12:00 -0600

    Initial setup

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+  if (!sequence) {
+    let sequence = [1, 1];
+  }
+  if (index <= sequence.length) {
+    return sequence[index];
+  } else {
+    sequence.push(
+      sequence[sequence.length] + sequence[sequence.length - 1],
+    );
+    fibonacci(index);
+  }
 };
 
 // Do not edit below this line
