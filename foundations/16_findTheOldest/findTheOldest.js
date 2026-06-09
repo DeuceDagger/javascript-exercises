235f1536c942fce817a45486efbaa038760a0a04
Josh Russell <255812605+DeuceDagger@users.noreply.github.com>
2026-06-09 17:56:52 -0600

    Initial trial

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let ages = [];
+  let thisYear = new Date().getFullYear;
 
+  for (const person of people) {
+    let birth = person.yearOfBirth;
+    let death = "yearOfDeath" in person ? person.yearOfDeath : thisYear;
+    ages.push(death - birth);
+  }
+  let oldestAge = ages.reduce((a, b) => Math.max(a, b), -Infinity);
+  let oldestIndex = ages.indexOf(oldestAge);
+  return people[oldestIndex];
 };
 
 // Do not edit below this line
