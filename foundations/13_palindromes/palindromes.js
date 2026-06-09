a0076db42ae73fe19bfa4173e34e5bb975afc417
Josh Russell <255812605+DeuceDagger@users.noreply.github.com>
2026-06-09 17:00:51 -0600

    Complete exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let normalString = string.replace(/[^a-zA-Z0-9]+/g, "").toUpperCase();
+  let reverseString = normalString
+    .split("")
+    .reverse()
+    .join("")
+    .toUpperCase();
+  return normalString === reverseString;
 };
 
 // Do not edit below this line
